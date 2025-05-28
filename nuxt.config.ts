87447438faa88869518ddc85ce21a2b188ae5b5a
oleksandr <slimnation2@gmail.com>
2025-05-28 13:15:33 +0200

    tailwind connected

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
-  i18n: {
-    locales: [
-      { code: 'en', name: 'English', file: 'en.json' },
-      { code: 'uk', name: 'Українська', file: 'uk.json' }  // краще не використовувати nl для української
-    ],
-    defaultLocale: 'en',
-    lazy: true,
-    langDir: 'locales/',
-  }
+  tailwindcss: {
+    exposeConfig: true,
+  },
+  css: ["~/assets/css/tailwind.css"],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
         <h1>Home</h1>
-        <NuxtLink to="/about" class="text-blue-500 hover:text-blue-700 bg-gray-100 p-2 rounded">About</NuxtLink>
+        <NuxtLink to="/about" class="text-blue-500 hover:text-blue-700 bg-primary p-2 rounded">About</NuxtLink>
     </div>
 </template>
